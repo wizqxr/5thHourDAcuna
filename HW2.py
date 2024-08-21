c2449aafec3d574930c796c9b08a137f6fc87cee
Dominic Acuna <da125401@student.wpsok.org>
2024-08-21 13:16:05 -0500

    Added HW2

---
+++ HW2.py
@@ -0,0 +1,19 @@
+#Name: Dominic Acuna
+#Class: 5th Hour
+#Assignment: HW2
+
+
+#Print Hello World!
+print("Hello World!")
+
+#print a question asking for your name and a way for you to respond
+name = input("What is your name?")
+print("Hello" + name + "!")
+
+#Have it ask for two integers from the user
+x = input("Give me a number")
+y = input("Give me another number")
+
+#Add the two integers together and print them out
+sum = int(x) + int(y)
+print("The sum of these numbers in" , sum)
