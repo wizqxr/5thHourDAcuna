96e8089d4cf58ba77ee01f5a842a3c81ef330f70
Dominic Acuna <da125401@student.wpsok.org>
2024-09-05 13:19:11 -0500

    Added Playground

---
+++ Playground.py
@@ -0,0 +1,29 @@
+
+name = (input("Hello, what is the name your choosing to be known as?"))
+print("Nice to meet you,",name ,",What shall we do today?")
+input()
+print("That sounds great to do", name, "!")
+print("Tell me a little about yourself!")
+input()
+print("Wow! Your a very interesting human being!")
+
+riddleList = ["Yes", "No"]
+print("Lemme tell you a riddle! Its a little odd but i'm sure you'll get it!")
+print("Listen here all far and wide, for I am the greatest computer of all! Do you Deny?!")
+
+x = input()
+if x == "No":
+    print("Good job! I thought I'd have to destroy your internet haha!")
+
+elif x == "Yes":
+    print("Sorry man but I gotta destroy your internet access. Goodbye :)")
+
+else:
+    print("You IDIOT! SAY Yes. OR No.")
+
+x = input()
+if x == "No":
+    print("Good job! I thought I'd have to destroy your internet haha!")
+
+elif x == "Yes":
+    print("Sorry man but I gotta destroy your internet access. Goodbye :)")
