b1419ea98f69e9dd6150bc4f916f0d69efc22825
Dominic Acuna <da125401@student.wpsok.org>
2024-10-03 13:15:54 -0500

    Added HW7

---
+++ HW7.py
@@ -0,0 +1,43 @@
+#Name: Dominic Acuna
+#Class: 5th Hour
+#Assignment: HW7
+
+#1. Print Hello World!
+print("Hello World!")
+
+#2. Create three different boolean variables named wifi, login, and admin.
+wifi = True
+login = True
+admin = True
+
+#3. Create a separate integer variable that denotes the number of times
+#someone with admin credentials has logged in.
+userValue = 0
+
+#4. Create a nested if statement that checks to see if wifi is true,
+#login is true, and admin is true. If they are all true, print a
+#welcome message and increase the integer variable by one. If one of them
+#is false, print an error message telling them which one they are "missing".
+x = input("Connect to Internet? ")
+
+if x == "No" or x == "no":
+    print("Goodbye! ")
+elif x == "Yes" or x == "yes":
+    print("Alright your connected! Enter your username! ")
+else:
+    print("Error, couldn't understand your answer. ")
+userName = input()
+
+if wifi == True:
+    if login == True:
+        if admin == True:
+         print("Welcome",userName,"!")
+         userValue += 1
+         print("Online Users: ",userValue)
+        else:
+            print("Error, you are missing admin access.")
+    else:
+        print("Error, you are missing the login credentials.")
+else:
+    print("Error, you are not connected to the internet.")
+
