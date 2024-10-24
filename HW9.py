3cfa24a77ce2e238c54a137a6cd80d3b771694fe
Dominic Acuna <da125401@student.wpsok.org>
2024-10-24 13:00:37 -0500

    Added HW9

--- HW9.py
+++ HW9.py
@@ -7,12 +7,13 @@ print("Hello World!")
 #2. Go to the link below and convert the code into pseudocode in comments, then code the flowchart completely:
 #https://adacomputerscience.org/images/content/computer_science/design_and_development/program_design/figures/ada_cs_design_flow_ifelseif.svg
 
-temp = 15
+temp = 20
 
+print("the temperature is", temp, "degrees celsius.")
 if temp >= 20:
     print("its hot")
 elif temp >= 10:
-    print("its cold")
+    print("its mild")
 else:
-    print("ITS FREEZING!!!")
+    print("it's cold")
 print("Thank You.")
