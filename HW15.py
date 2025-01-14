a7d103d310d117dd17b157f298e52e22a992ff73
Dominic Acuna <da125401@student.wpsok.org>
2025-01-14 13:10:23 -0600

    HW15

---
+++ HW15.py
@@ -0,0 +1,35 @@
+#Name: Dominic Acuna
+#Class: 5th Hour
+#Assignment: HW15
+import random
+import time
+
+
+#1. Create a def function that prints out "Hello World!"
+def hello_world():
+    print("Hello World!")
+
+#2. Create a def function that calculates the average of three numbers.
+def calculate_average(num1, num2, num3):
+
+    average = (num1 + num2 + num3) / 3
+    print(num1, num2, num3)
+    print(average)
+
+#3. Create a def function with the names of 5 animals as arguments, treats it like a list, and
+#prints the name of the third animal.
+def animal_list(*fuhuh):
+    print("This animal is", fuhuh[2])
+
+#4. Create a def function that loops from 1 to the number put in the argument.
+def print_numbers(n):
+
+  for i in range(1, n + 1):
+    time.sleep(0.4)
+    print(i)
+
+#5. Call all of the functions created in 1 - 4 with relevant arguments.
+hello_world()
+calculate_average(random.randint(1, 100), random.randint(1, 100), random.randint(1, 100))
+animal_list("Dog", "Cat", "Pig", "Horse", "Zach")
+print_numbers(5)
---
+++ Scenario 5.py
@@ -0,0 +1,28 @@
+#Name: Zachary Martin and Dominic Acuna
+#Class: 5th Hour
+#Assignment: Scenario 5
+
+#Scenario 5
+#You're all part of a new development team and the big wigs want to see what you are all capable of.
+#They want you to develop whatever you want to develop, but there's only one problem:
+#the same big wigs only bought enough computers for half of you. Because of this, you will
+#all be split into teams of two. One serving as the brain (not using the computer),
+#one serving as the hands (using the computer).
+
+#The Teams are as such:
+
+#Team 1: Dom (brain), Zachary (hand)
+#Team 2: Ryley (brain), Ethan W (hand)
+#Team 3: Eli (brain), Preston (hand)
+#Team 4: Gabe (brain), Quinn (hand)
+#Team 5: Sam (brain), Chaysen (hand)
+#Team 6: Kevin (brain), Ethan M (hand)
+#Team 7: Gage (brain), Eric (hand)
+
+
+#You have until the Scenario is due to brainstorm an idea together, create a flowchart, and then
+#turn that flowchart into functioning code. The code itself must have at least: 1 dictionary or list,
+#1 loop, 2 if statements (elif and else statements tied to it does not count towards the total),
+#1 variable with a user input, and 1 form of an assignment operator. You are free to add whatever else
+#you feel is necessary to complete your concept. You will be graded based on how those ideas are
+#implemented together.
