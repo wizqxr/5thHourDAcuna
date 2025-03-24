e457397f1747342d0449ee61add82ee9d8415d15
Dominic Acuna <da125401@student.wpsok.org>
2025-03-24 12:54:35 -0500

    added edits to hw22

--- HW22.py
+++ HW22.py
@@ -15,9 +15,11 @@ ramen = Store(20, 5.47, 1)
 rice = Store(25, 3.47, 1)
 beans = Store(20, 4.47, 2)
 #3. Print the stock of all three objects and the cost of the second store item.
-print(f"The cost of Ramen is: {ramen.cost}")
-print(f"The cost of Rice is: {rice.cost}")
-print(f"The cost of Beans is: {beans.cost}")
+print(f"The stock of Ramen is: {ramen.stock}")
+print(f"The stock of Rice is: {rice.stock}")
+print(f"The stock of Beans is: {beans.stock}")
+
+print(f"The cost of rice is: {rice.cost}")
 #4. Make a def function within the class that doubles the cost an item, double the cost of the second store item, and print the new cost below the original cost print statement.
 rice.double()
 print("The doubled cost of item is:", rice.cost)
